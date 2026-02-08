cd7aeca6621f5d830accfac56804a63c215ae731
Vinicius De Oliveira <vinnyuniverso3@gmail.com>
2026-02-08 09:28:59 -0300

    feature: add all model classes

---
+++ src/main/java/com/linus/model/Aluno.java
@@ -0,0 +1,34 @@
+package com.linus.model;
+
+import lombok.Data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Classe modelo que abstrai registros da tabela {@code ALUNO}
+ */
+@Data
+public class Aluno {
+
+    private long matricula;
+    private String email;
+    private String nome;
+    private String cpf;
+    private String hashSenha;
+    private long id_turma;
+
+    /**
+     * Construtor da classe que utiliza diretamente objeto {@code ResultSet}.
+     * @param rs Objeto {@code ResultSet} resultante da consulta ao banco de dados.
+     * @throws SQLException Caso falha SQL
+     */
+    public Aluno(ResultSet rs) throws SQLException {
+        this.matricula = rs.getLong("matricula");
+        this.email = rs.getString("email");
+        this.nome = rs.getString("nome");
+        this.cpf = rs.getString("cpf");
+        this.hashSenha = rs.getString("hash_senha");
+        this.id_turma = rs.getLong("id_turma");
+    }
+}
