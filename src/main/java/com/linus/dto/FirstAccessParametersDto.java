64e97e98af60af8e411c27b7270e7ea8ee2c8d5a
Vinicius De Oliveira <vinnyuniverso3@gmail.com>
2026-02-24 07:12:45 -0300

    feature: new DTO class for 'FisrtsAccessServlet' class

---
+++ src/main/java/com/linus/dto/FirstAccessParametersDto.java
@@ -0,0 +1,13 @@
+package com.linus.dto;
+
+/**
+ * Classe de DTO que abstrai os parametros de requicao para servlet {@code FirstAcessServlet}
+ */
+public class FirstAccessParametersDto {
+
+    public String nome;
+    public String email;
+    public String senha;
+    public String cpf;
+    public long matricula;
+}
