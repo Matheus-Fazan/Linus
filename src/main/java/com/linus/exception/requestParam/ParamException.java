c1fd54cf5614d1d0c869a40de7ef97b84d13fcbf
Vinicius De Oliveira <vinnyuniverso3@gmail.com>
2026-02-25 21:23:27 -0300

    feature: Add param exception classes

--- src/main/java/com/linus/exception/requestParam/ParamException.java
+++ src/main/java/com/linus/exception/requestParam/ParamException.java
@@ -1,9 +1,11 @@
 package com.linus.exception.requestParam;
 
-/**
- * Classe de excessão genérica de erro de validação nos parâmetros de requisição
- */
-public sealed class ParamException extends RuntimeException permits EmptyParamException, OutOfBoundsParamException, OutOfPatternCpfException, OutOfPatternEmailException, OutOfPatternPasswordException
+public sealed class ParamException extends RuntimeException permits
+        EmptyParamException,
+        OutOfBoundsParamException,
+        OutOfPatternCpfException,
+        OutOfPatternEmailException,
+        OutOfPatternPasswordException
 {
 
     public ParamException(String message) {
