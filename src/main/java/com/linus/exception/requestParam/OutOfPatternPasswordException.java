aeeea86bd66d3e44223d3ae679941999c2748de3
Vinicius De Oliveira <vinnyuniverso3@gmail.com>
2026-02-25 22:08:56 -0300

    fix: updating contructor method of OutOfPatternPasswordException class to receive no parameters

--- src/main/java/com/linus/exception/requestParam/OutOfPatternPasswordException.java
+++ src/main/java/com/linus/exception/requestParam/OutOfPatternPasswordException.java
@@ -4,7 +4,7 @@ public final class OutOfPatternPasswordException extends ParamException{
 
     private static final String MESSAGE = "Campo de 'senha' deve conter no mínimo 8 caracteres e ao menos um carácter maiúsculo e numérico. Por favor, insira um valor válido.";
 
-    public OutOfPatternPasswordException(String message) {
-        super(message);
+    public OutOfPatternPasswordException() {
+        super(MESSAGE);
     }
 }
