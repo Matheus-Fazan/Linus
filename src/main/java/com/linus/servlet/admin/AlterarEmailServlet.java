1676884c3e650de7d226a70463e6618d19ec8ec2
Neveslv <enzoematheus092009@gmail.com>
2026-03-02 11:01:09 -0300

    fix(servlet): correct forward path to /WEB-INF/pages/admin/editarAluno.jsp

--- src/main/java/com/linus/servlet/admin/AlterarEmailServlet.java
+++ src/main/java/com/linus/servlet/admin/AlterarEmailServlet.java
@@ -47,7 +47,7 @@ public class AlterarEmailServlet extends HttpServlet {
             requestReponse.addRequestAttribute("error", "Nenhum registro foi alterado. Por favor, verifique a matrícula informada.");
 
         } finally {
-            requestReponse.forwardTo("/../editarAluno.jsp");
+            requestReponse.forwardTo("/WEB-INF/pages/admin/editarAluno.jsp");
         }
     }
 }
