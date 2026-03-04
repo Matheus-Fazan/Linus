4300f7723ec21d5b54561f6410941c3daf1ea837
Neveslv <enzoematheus092009@gmail.com>
2026-03-04 11:12:14 -0300

    fix(servlet): update forward path to /WEB-INF/pages/admin/criarAcesso.jsp

--- src/main/java/com/linus/servlet/admin/adicionarAlunoServlet.java
+++ src/main/java/com/linus/servlet/admin/adicionarAlunoServlet.java
@@ -47,7 +47,7 @@ public class adicionarAlunoServlet extends HttpServlet {
             requestReponse.addRequestAttribute("error", "Falha ao adicionar pré-cadastro. Por favor, tente novamente.");
 
         } finally {
-            requestReponse.forwardTo("/../criarAcesso.jsp");
+            requestReponse.forwardTo("/WEB-INF/pages/admin/criarAcesso.jsp");
         }
     }
 }
