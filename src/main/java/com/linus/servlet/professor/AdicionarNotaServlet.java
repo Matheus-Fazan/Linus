f621dd5ad03e6a7dba1aca9e6c602446d8a322bd
Neveslv <enzoematheus092009@gmail.com>
2026-03-04 23:02:29 -0300

    feat(servlet): implement doGet method to load nota form page

--- src/main/java/com/linus/servlet/professor/AdicionarNotaServlet.java
+++ src/main/java/com/linus/servlet/professor/AdicionarNotaServlet.java
@@ -1,4 +1,28 @@
 package com.linus.servlet.professor;
 
-public class AdicionarNotaServlet {
+import com.linus.dao.NotaDao;
+import com.linus.dto.NotaDto;
+import com.linus.exception.dao.ConnectionException;
+import com.linus.exception.dao.NoRegistersAlteredException;
+import com.linus.exception.requestParam.ParamException;
+import com.linus.model.servlet.RequestReponse;
+import com.linus.validation.Validator;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+
+@WebServlet("/professor/adicionar-nota")
+public class AdicionarNotaServlet extends HttpServlet {
+
+    private static final NotaDao dao = new NotaDao();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/pages/professor/adicionarNota.jsp").forward(req, resp);
+    }
 }
