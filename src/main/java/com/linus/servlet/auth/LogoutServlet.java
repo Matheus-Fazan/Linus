c38fb33debe61c75ce3d12ccc78854fabab84aa8
luismede <luishenrique23hh23@gmail.com>
2026-03-04 22:04:54 -0300

    feat: implement password recover with verification and reset functionality

--- src/main/java/com/linus/servlet/auth/LogoutServlet.java
+++ src/main/java/com/linus/servlet/auth/LogoutServlet.java
@@ -1,6 +1,7 @@
 package com.linus.servlet.auth;
 
 import com.linus.model.servlet.RequestReponse;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,20 +12,18 @@ import java.io.IOException;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
-
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
-        resp.setHeader("Allow", "POST");
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestReponse requestReponse = new RequestReponse(req, resp);
         HttpSession session = req.getSession(false);
         if (session != null) {
             session.invalidate();
         }
         requestReponse.redirectTo("/index.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
     }
 }
